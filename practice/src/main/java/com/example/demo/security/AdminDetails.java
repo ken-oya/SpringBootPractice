5a58a0d456a78a4befe669dc486540a87790199d
Kentaro Oya <k.oya.cingroup2012@gmail.com>
2025-06-27 21:00:35 +0900

    最終課題 その4

---
+++ practice/src/main/java/com/example/demo/security/AdminDetails.java
@@ -0,0 +1,42 @@
+package com.example.demo.security;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.entity.Admin;
+
+public class AdminDetails implements UserDetails {
+
+    private final Admin admin;
+
+    public AdminDetails(Admin admin) {
+        this.admin = admin;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return admin.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return admin.getEmail();
+    }
+
+    @Override public boolean isAccountNonExpired()     { return true; }
+    @Override public boolean isAccountNonLocked()      { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled()               { return true; }
+}
